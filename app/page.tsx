2f47e62502f7bea84a937592de7f8c9bb036ae4b
shirocodes <ishirapt@gmail.com>
2025-10-29 11:11:26 +0300

    Add LightRays component and update layout and styles
    - Introduced LightRays component for dynamic visual effects.
    - Updated layout to include LightRays with specified properties.
    - Enhanced globals.css with additional CSS variables for theming.
    - Revised page.tsx to display event-related content.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-
-
 const page = () => {
   return (
-    <div>A new journey begins!</div>
+    <section>
+      <h1 className="text-center">
+        Every developer's event companion <br /> 
+        An Event You cannot Miss!
+      </h1>
+      <p className="text-center mt-4">
+        Hackathons, Meetups, and Conferences, all in one place.
+      </p>
+    </section>
   )
 }
 
